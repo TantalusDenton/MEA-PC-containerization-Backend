d2f67345cd962d71000c2451548ee9fbef82f3b2
TantalusDenton <thomasdxdenton@hotmail.com>
2018-05-17 22:45:49 +0000

    fixed main_test.go and run test successfully

--- main_test.go
+++ main_test.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestHandler(t *testing.T) {
+func TestConnectToLXDserver(t *testing.T) {
 
-	request := events.APIGatewayProxyRequest{}
-	expectedResponse := events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Headers: map[string]string{
-			"Content-Type": "text/html",
-		},
-		Body: "Congratulations",
+	err := connectToLXDserver()
+
+	if err != nil {
+		t.Log("OK")
+	} else {
+		t.Log("not OK")
 	}
-
-	response, err := Handler(request)
-
-	assert.Equal(t, response.Headers, expectedResponse.Headers)
-	assert.Contains(t, response.Body, expectedResponse.Body)
-	assert.Equal(t, err, nil)
 
 }
