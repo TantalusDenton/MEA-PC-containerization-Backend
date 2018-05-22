294f798b514e3169a0db715c390af236a981e502
TantalusDenton <thomasdxdenton@hotmail.com>
2018-05-22 00:23:43 +0000

    added aws sdk and used it to download certs from s3

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/lxc/lxd/client"
 	"github.com/lxc/lxd/shared/api"
 )
@@ -14,18 +19,64 @@ func checkFileForError(e error) {
 	}
 }
 
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
+//GetFilesFromS3 is a reusable function. Just call it and tell it which files to download.
+func GetFileFromS3(S3itemToDOwnload string) {
+
+	bucket := "lxd-server-certificates"
+	item := S3itemToDOwnload
+
+	file, err := os.Create(item)
+	if err != nil {
+		exitErrorf("Unable to open file %q, %v", err)
+	}
+
+	defer file.Close()
+
+	// Initialize a session in us-east-1.
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+
+	downloader := s3manager.NewDownloader(sess)
+
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(item),
+		})
+	if err != nil {
+		exitErrorf("Unable to download item %q, %v", item, err)
+	}
+
+	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+}
+
 func connectToLXDserver() error {
 
-	// Connection parameters - LXD API needs to know client vert, key and server cert
-	ClientCertFile, errcert := ioutil.ReadFile("/home/tantalus/lxd-api-access-cert-key-files/lxd-type3access.crt")
+	//declare certs for passing to AWS S3 function
+	serverCertFromS3 := "ec2-lxd-server-for-go-api.crt"
+	clientCertFromS3 := "lxd-type3access.crt"
+	clientKeyFromS3 := "lxd-type3access.key"
+
+	GetFileFromS3(serverCertFromS3)
+	GetFileFromS3(clientCertFromS3)
+	GetFileFromS3(clientKeyFromS3)
+
+	// Connection parameters - LXD API needs to know client cert, key and server cert
+	ClientCertFile, errcert := ioutil.ReadFile(clientCertFromS3)
 	checkFileForError(errcert)
 	ClientCertString := string(ClientCertFile)
 
-	ClientKeyFile, errkey := ioutil.ReadFile("/home/tantalus/lxd-api-access-cert-key-files/lxd-type3access.key")
+	ClientKeyFile, errkey := ioutil.ReadFile(clientKeyFromS3)
 	checkFileForError(errkey)
 	ClientKeyString := string(ClientKeyFile)
 
-	ServerCertFile, errservercert := ioutil.ReadFile("/home/tantalus/lxd-api-access-cert-key-files/lxd-type3server.crt")
+	ServerCertFile, errservercert := ioutil.ReadFile(serverCertFromS3)
 	checkFileForError(errservercert)
 	ServerCertString := string(ServerCertFile)
 
@@ -36,7 +87,7 @@ func connectToLXDserver() error {
 		/*InsecureSkipVerify: true*/}
 
 	// Connect to LXD over http
-	c, err := lxd.ConnectLXD("https://127.0.0.1:8443", argumentsToPass)
+	c, err := lxd.ConnectLXD("https://172.30.2.21:8443", argumentsToPass)
 	if err != nil {
 		fmt.Print("Could not connect because of error: ", err)
 		fmt.Print("server cert is: ", ServerCertString)
