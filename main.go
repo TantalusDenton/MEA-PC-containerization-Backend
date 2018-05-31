853f18c96b23fef4d17eb1ca8b92420a66938dfb
TantalusDenton <thomasdxdenton@hotmail.com>
2018-05-31 19:22:18 +0000

    removed static credentials

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws/credentials"
+	//"github.com/aws/aws-sdk-go/aws/credentials"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -54,7 +54,7 @@ func GetFileFromS3(S3itemToDOwnload string) {
 	// Initialize a session in us-east-1.
 	sess, err := session.NewSession(&aws.Config{
 		Region:      aws.String("us-east-1"),
-		Credentials: credentials.NewStaticCredentials(KeyID, SecretKey, TokenForSession)},
+		/*Credentials: credentials.NewStaticCredentials(KeyID, SecretKey, TokenForSession)*/},
 	)
 	if err != nil {
 		log.Print("Could not start download session for S3 because of: ", err)
